6aaa83ccbce85e342fa011525fc7e4dcc082f102
Daniel <dafemart@ucsc.edu>
2018-01-17 23:10:11 -0800

    typo fixed

--- median_two_sorted_arrays/main.cpp
+++ median_two_sorted_arrays/main.cpp
@@ -17,13 +17,12 @@ float median(int arr1[], int arr2[], int size_1, int size_2){
    float m1 = median_h(arr1,size_1);
    float m2 = median_h(arr2,size_2);
    float median_res = 0.0;
-
    if(m1 == m2)
-      median_res = m1;
+      return m1;
    else{
      int index = 0;
      int size = ceil((float)size_1/(float)2);
-     (size_1 % 2 == 0)? index = size_1/2 : index = floor(size_1/2); 
+     (size_1 % 2 == 0)? index = size_1/2 + 1 : index = size_1/2; 
      if(m1 < m2)
        median_res = median(arr1 + index,arr2, size, size);    
      else
@@ -35,8 +34,8 @@ float median(int arr1[], int arr2[], int size_1, int size_2){
 
 
 int main(void){
-    int arr1[] = {1,2,3,6};
-    int arr2[] = {4,6,8,10};
+    int arr1[] = {900};
+    int arr2[] = {5,8,10,20};
 
     float med = median(arr1,arr2,5,5);
     cout << med << endl;
